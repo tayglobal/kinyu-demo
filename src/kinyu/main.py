16b86ec7d3ed8388d4425b8d3de45b69d48a3b89
Tony Yum <tony.yum@gmail.com>
2020-08-27 13:58:23 +0000

    Added main

--- src/kinyu/main.py
+++ src/kinyu/main.py
@@ -1,20 +1,19 @@
 import click
 import importlib
+import json
 from kinyu.rimport.api import RemoteImporter
 
 
 @click.command()
 @click.option('--srcdb', help='The source db')
+@click.option('--entry', default='main', help='The entry function')
+@click.option('--args', default='{}', help='Arguments to be passed into entry function in JSON format')
 @click.argument('module_path')
-def run(srcdb, module_path):
+def run(srcdb, entry, args, module_path):
     ri = RemoteImporter(srcdb)
-    ri.add_script("unittest/main_module.py", '''
-def main():
-    print("Hello World")
-''')
     ri.install()
     m = importlib.import_module(module_path)
-    m.main()
+    getattr(m, entry)(**json.loads(args))
 
 
 if __name__ == '__main__':
--- src/kinyu/tests/test_main.py
+++ src/kinyu/tests/test_main.py
@@ -1,18 +1,51 @@
 from kinyu.rimport.api import RemoteImporter
 from subprocess import Popen, PIPE
 import os
+import pytest
+import json
 
 DYNAMODB = os.environ['KINYU_UNITTEST_DYNAMODB']
+SRCDB = 'dynamodb://' + DYNAMODB
 
 
-def test_main():
-    srcdb = 'dynamodb://' + DYNAMODB
-    ri = RemoteImporter(srcdb)
-    ri.add_script("unittest/main_module.py", '''
+@pytest.fixture
+def remote_importer():
+    ri = RemoteImporter(SRCDB)
+    return ri
+
+
+def _run_test(args):
+    command = ["python", "-m", "kinyu.main", "--srcdb=" + SRCDB] + args
+    with Popen(command, stdout=PIPE) as proc:
+        output = proc.stdout.read()
+    assert proc.returncode == 0
+    return output
+
+
+def test_main(remote_importer):
+    remote_importer.add_script("unittest/main_module.py", '''
 def main():
     print("Hello World")
 ''')
-    command = ["python", "-m", "kinyu.main",
-               "--srcdb=" + srcdb, "unittest.main_module"]
-    with Popen(command, stdout=PIPE) as proc:
-        assert proc.stdout.read() == b'Hello World\n'
+    assert _run_test(["unittest.main_module"]) == b'Hello World\n'
+
+
+def test_entry(remote_importer):
+    remote_importer.add_script("unittest/test_entry.py", '''
+def test_entry():
+    print("Welcome")
+''')
+    assert _run_test(
+        ["unittest.test_entry", "--entry=test_entry"]) == b'Welcome\n'
+
+
+def test_args(remote_importer):
+    remote_importer.add_script("unittest/test_args.py", '''
+def main(a, b, c=3):
+    assert a == 1
+    assert b == 2
+    assert c == 3
+''')
+
+    args = {'a': 1, 'b': 2}
+    _run_test(["unittest.test_args", '--args=' + json.dumps(args)])
